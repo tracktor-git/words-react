0d25b33dacfd147a32100feaecf48a49a3f6604c
Tracktor <tracktor@bk.ru>
2023-11-26 17:10:29 +0300

    Modify polyfills

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,21 +1,18 @@
 /* eslint-disable no-extend-native */
 
+function at(index) {
+  if (index >= 0) {
+    return this[index];
+  }
+  return this[this.length + index];
+}
+
 if (Array.prototype.at === undefined) {
-  Array.prototype.at = function at(index) {
-    if (index >= 0) {
-      return this[index];
-    }
-    return this[this.length + index];
-  };
+  Array.prototype.at = at;
 }
 
 if (String.prototype.at === undefined) {
-  String.prototype.at = function at(index) {
-    if (index >= 0) {
-      return this[index];
-    }
-    return this[this.length + index];
-  };
+  String.prototype.at = at;
 }
 
 if (String.prototype.replaceAll === undefined) {
